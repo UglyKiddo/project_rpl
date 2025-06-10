8475a1830c83f53f76cee035de92431310f1d91f
gogopowercoding <furaihanalharits@gmail.com>
2025-06-10 09:50:26 +0700

    ss

---
+++ peneliti/homePi.php
@@ -0,0 +1,15 @@
+include 'partials/header.php';
+include "partials/footer.php";
+
+
+<div class="main">
+  <a href="index.php?page=peternakan" class="category-btn">
+    <i class="fa-solid fa-cow"></i>Peternakan
+  </a>
+  <a href="index.php?page=perikanan" class="category-btn">
+    <i class="fa-solid fa-fish"></i>Perikanan
+  </a>
+  <a href="index.php?page=pertanian" class="category-btn">
+    <i class="fa-solid fa-wheat-awn"></i>Pertanian
+  </a>
+</div>
